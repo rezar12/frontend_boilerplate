17a5c38b7f94a2efff79a165cd8b768a4a0e2938
Devrezard <devunbutu@gmail.com>
2025-03-19 08:44:23 +0000

    change

---
+++ apps/console_app/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import type React from "react"
+ 
+export default async function Page() {
+ 
+  return (
+    <main>
+        <div>
+            <h1>Dashboard</h1>
+        </div>
+    </main>
+  )
+}
